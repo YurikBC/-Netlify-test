86cfde4d85157ebefac19eef206d5b72bdd85973
yury.kryshchuk <yury.kryshchuk@exposit.com>
2017-12-20 00:06:20 +0300

    yuriy commit

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,8 @@
         nuxt-test
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Yuriy Mazafaka project
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
